3dc5232acaf21a194aa7ce38411a73f2d411a801
EC2 Default User <ec2-user@ip-172-31-37-33.us-east-2.compute.internal>
2023-09-08 03:55:06 +0000

    from prod

--- static/js/liveDashboard.js
+++ static/js/liveDashboard.js
@@ -11,7 +11,7 @@ function updateTable(data) {
   tr.innerHTML = `
   <td colspan="8">No approved bets available right now.</td>`;
   tableBody.appendChild(tr);
-  boolin = false;
+  boolin = true;
   if (!(data.length === 1 && data[0].update === false) && boolin) {
     const tableBody = document.querySelector('#data-table tbody');    tableBody.innerHTML = '';
 
@@ -26,7 +26,7 @@ function updateTable(data) {
       <td before-data="Sportsbook with Odds">${row.sportsbooks_used}</td>
       <td class="mobile-no-display">${row.date}</td>
       <td before-data="Time Since Odds Update: ">${row.time_difference_formatted}</td>
-      <td before-data="Recommended Bet Size: ">${row.bet_amount}</td>
+      <td before-data="Recommended Bet Size: ">$${row.bet_amount}</td>
       <td data-title="button"><button onclick="editRow(this)" class="add-to-betslip-button" id="add-to-betslip-button" data-ev="${row.ev}" data-team="${row.team}" data-odds="${row.highest_bettable_odds}">Add to My Bets</button></td>
       `;
       console.log(row)
