b34ae079f25f9723b3e8807a8719157ac979a956
micahcb <micahcb@umich.edu>
2023-09-21 18:58:26 -0400

    added the login block functionality

--- functionality/database.py
+++ functionality/database.py
@@ -9,6 +9,7 @@ import numpy as np
 from flask import jsonify
 import math
 import datetime
+from datetime import datetime
 import ast
 import sqlite3
 import stripe
@@ -39,7 +40,20 @@ class database():
        new_user.create_user(firstname, lastname, username, password, phone, bankroll, sign_up_date, payed)
 
        self.users = self.get_all_usernames()
-
+    def check_account(self,username):
+      conn = self.make_conn()
+      df = pd.read_sql('SELECT * FROM login_info', conn)
+      user_info = df[df['username'] == username]
+      time_difference = datetime.now() - datetime.strptime(user_info['date_signed_up'].item(), '%Y-%m-%d %H:%M:%S.%f')
+    
+      days_difference = time_difference.days
+      if user_info['payed'].item() or (days_difference <= 8):
+        conn.close()
+        return True
+      conn.close()
+      return False
+      
+       
     def check_login_credentials(self, username, password):
       #df = pd.read_csv('users/login_info.csv')
       conn = self.make_conn()
@@ -52,6 +66,27 @@ class database():
           return True
       conn.commit()  # Commit the changes
       conn.close()   # Close the connection
+    
+    def check_duplicate_account(self,username):
+        self.check_payments()
+        conn = self.make_conn()
+        df = pd.read_sql('SELECT * FROM login_info', conn)
+        user_info = df[df['username'] == username]
+        if user_info['payed'].item():
+          print(user_info['payed'].item())
+          #remove this row from the df and push it back to sqllite
+          df = df[df['username'] != username]
+          df.to_sql('login_info', conn, if_exists='replace', index=False)
+          conn.commit()
+          conn.close()
+          return True
+        else:
+          
+          conn.commit()
+          conn.close()
+          return False
+
+
 
     def make_data(self, strategy_name):
         full_df = self.get_data(strategy_name)
@@ -362,6 +397,23 @@ class database():
       conn.close() 
       return
     
+    def calculate_accepted_bettable_odds(row):
+        value_new = row['highest_bettable_odds']
+        if value_new < 0:
+          if value_new < -500:
+             value_new = value_new - (value_new * 0.1)
+          else:
+             value_new = value_new - (value_new * 0.05)
+        else:
+          if value_new > 500:
+             value_new = value_new + (value_new * 0.1)
+          else:
+             value_new = value_new + (value_new * 0.05)
+        row['highest_acceptable_odds'] = value_new
+        return row
+        
+      
+    
     def get_live_dash_data(self, user_name):
        df = pd.read_csv('users/model_obs.csv')
        conn = self.make_conn()
@@ -389,7 +441,9 @@ class database():
        
        first_20_rows = df_no_duplicates.head(20)
 
-       current_time = datetime.datetime.now() 
+       first_20_rows= first_20_rows.apply(self.calculate_accepted_bettable_odds, axis=1)
+      
+       current_time = datetime.now() 
 
        first_20_rows['current_time'] = current_time #+ pd.Timedelta(hours=6)
 
@@ -483,6 +537,8 @@ class database():
        current_time = datetime.datetime.now() - datetime.timedelta(hours=7)
 
        first_20_rows['current_time'] = current_time 
+       first_20_rows = first_20_rows.apply(self.calculate_accepted_bettable_odds, axis=1)
+      
 
        first_20_rows['snapshot_time'] = first_20_rows['snapshot_time'].apply(pd.to_datetime)
 
@@ -749,7 +805,7 @@ class database():
 
        user_df['time_placed'] = pd.to_datetime(user_df['time_placed'])
 
-       user_time_df = user_df[(datetime.datetime.now()- user_df['time_placed']  < pd.Timedelta(seconds=300))]
+       user_time_df = user_df[(datetime.now()- user_df['time_placed']  < pd.Timedelta(seconds=300))]
 
        if sport == "NFL":
           user_time_df['teams_bet_on'] = user_time_df['team'].str.split('v.').str[0]
@@ -789,6 +845,7 @@ class database():
       except sqlite3.Error as e:
             print(f"SQLite Error: {e}")
       conn.close()
+      return
 
 
       
