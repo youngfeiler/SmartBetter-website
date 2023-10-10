4cb3803bffaee9b6b5ae1d01de94b4c8626a3662
youngfeiler <stefantfeiler@gmail.com>
2023-10-09 18:46:45 -0600

    removed more prints

--- functionality/tasks.py
+++ functionality/tasks.py
@@ -76,7 +76,7 @@ def start_dashboard_runner():
 
       live_dashboard_runner_instance.make_live_dash_data()
       
-      # live_nfl_dashboard_runner_instance.make_live_dash_data()
+      live_nfl_dashboard_runner_instance.make_live_dash_data()
 
       observation_compiler_instace.compile_observations()
       observation_compiler_instace.update_completed_observations()
