878feb7ab1394e7649717acd1fc5c16586b439bc
youngfeiler <stefantfeiler@gmail.com>
2023-10-09 18:29:22 -0600

    removed a local path variable lol

--- functionality/live_dashboard_runner.py
+++ functionality/live_dashboard_runner.py
@@ -61,13 +61,10 @@ class live_dashboard_runner():
         market_odds_df = get_odds()
         combined_market_extra_df = preprocess(market_odds_df)
 
-        print('--------------------')
         
         self.market_odds = combined_market_extra_df
         self.stacked_df = make_stacked_df(combined_market_extra_df)
 
-        print('---------------------')
-        self.stacked_df.to_csv('/Users/stefanfeiler/Desktop/stacked_bets.csv')
 
         for strategy_name, strategy_dict in self.model_storage.items():
             this_model_raw_data_point = strategy_dict['data_collector'].format(self.stacked_df)
