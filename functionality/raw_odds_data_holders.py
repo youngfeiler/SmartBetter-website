702b937bbffb3864f1e541704f6deb780139426d
youngfeiler <stefantfeiler@gmail.com>
2023-10-30 14:17:58 -0600

    nothing

--- functionality/raw_odds_data_holders.py
+++ functionality/raw_odds_data_holders.py
@@ -8,42 +8,20 @@ class RawOddsHolders():
     
 
   def initialize_nfl_data(self):
-    odds_data = pd.read_parquet('/Users/stefanfeiler/Desktop/SMARTBETTOR_CODEBASE/odds_data/nfl_raw_odds_data.parquet')
-    scores_data = pd.read_csv('../extra_info_sheets/nfl_extra_info.csv')
+    odds_data = pd.read_parquet('/Users/stefanfeiler/Desktop/SMARTBETTOR_CODEBASE/odds_data/nfl_raw_odds_data_stacked.parquet')
+    
 
-    odds_data = odds_data.dropna(subset=['team_1'])
 
-    odds_data["commence_date"] = pd.to_datetime(odds_data["commence_time"]).dt.date.astype(str)
-    odds_data['hour_of_start'] = pd.to_datetime(odds_data["commence_time"]).dt.hour.astype(str)
-    odds_data["my_game_id"] =odds_data['team_1'] + odds_data['team_2'] + odds_data["commence_date"]
-
-    scores_data['commence_date'] = pd.to_datetime(scores_data['date']).dt.date.astype(str)
-
-    scores_data['my_game_id'] = scores_data['team_1'] + scores_data['team_2'] + scores_data["commence_date"]
-     
-    merged_df = pd.merge(odds_data, scores_data, on='my_game_id', how='outer', suffixes=('', '_df2'))
-
-    self.raw_nfl_odds_data = merged_df
+    self.raw_nfl_odds_data = odds_data
 
     return
 
   def initialize_nba_data(self):
-    odds_data = pd.read_parquet('/Users/stefanfeiler/Desktop/SMARTBETTOR_CODEBASE/odds_data/nba_raw_odds_data.parquet')
-    scores_data = pd.read_csv('../extra_info_sheets/nba_extra_info.csv')
+    odds_data = pd.read_parquet('/Users/stefanfeiler/Desktop/SMARTBETTOR_CODEBASE/odds_data/nba_raw_odds_data_stacked.parquet')
+    
 
-    odds_data = odds_data.dropna(subset=['team_1'])
-
-    odds_data["commence_date"] = pd.to_datetime(odds_data["commence_time"]).dt.date.astype(str)
-    odds_data['hour_of_start'] = pd.to_datetime(odds_data["commence_time"]).dt.hour.astype(str)
-    odds_data["my_game_id"] =odds_data['team_1'] + odds_data['team_2'] + odds_data["commence_date"]
-
-    scores_data['commence_date'] = pd.to_datetime(scores_data['date']).dt.date.astype(str)
-
-    scores_data['my_game_id'] = scores_data['team_1'] + scores_data['team_2'] + scores_data["commence_date"]
-     
-    merged_df = pd.merge(odds_data, scores_data, on='my_game_id', how='outer', suffixes=('', '_df2'))
-
-    self.raw_nba_odds_data = merged_df
+  
+    self.raw_nba_odds_data = odds_data
 
     return
     
