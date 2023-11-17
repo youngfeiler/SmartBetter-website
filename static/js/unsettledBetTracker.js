a035360046e5e968f50366909a7d31512f5de786
youngfeiler <stefantfeiler@gmail.com>
2023-11-17 16:09:24 -0700

    final push for the day

--- static/js/unsettledBetTracker.js
+++ static/js/unsettledBetTracker.js
@@ -1,51 +1,91 @@
+function formatCurrency(number) {
+  return new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+  }).format(number);
+}
+
 // Assuming you fetch data and format it correctly
 fetch('/get_unsettled_bet_data')
   .then(response => response.json())
   .then(data => {
     console.log(data)
-    const tableBody = document.querySelector('#table-body');
+    const tableBody = document.querySelector('#bets-container');
     let previousGameId = null;
 
     for (const game_id in data) {
+
+      const betBox = document.createElement('div');
+      betBox.classList.add("bet");
+      
+
       const rows = data[game_id];
+
+      const headerBox = document.createElement('div');
+      headerBox.classList.add("header-box");
+      teamHeader = document.createElement('h3');
+      teamHeader.textContent = "Team";
+
+      result = document.createElement('h3');
+      result.textContent = "Payout";
+
+      avgOdds = document.createElement('h3');
+      avgOdds.textContent = "Avg Odds";
+
+      headerBox.appendChild(teamHeader);
+      headerBox.appendChild(result);
+      headerBox.appendChild(avgOdds);
+
+      betBox.appendChild(headerBox);
 
       for (let i = 0; i < rows.length; i++) {
         const row = rows[i];
-        const tableRow = document.createElement('tr');
 
-        const teamCell = document.createElement('td');
-        teamCell.textContent = row.team;
-        tableRow.appendChild(teamCell);
-
-        const ifWinCell = document.createElement('td');
-        ifWinCell.textContent = row.if_win;
-        tableRow.appendChild(ifWinCell);
-
-        const avgOddsCell = document.createElement('td');
-        avgOddsCell.textContent = row.average_odds;
-        tableRow.appendChild(avgOddsCell);
-
-        const highestOddsCell = document.createElement('td');
-        highestOddsCell.textContent = row.highest_odds;
-        tableRow.appendChild(highestOddsCell);
-
-        // Add a border class to the first row of each game_id pair
-        if (game_id !== previousGameId) {
-          tableRow.classList.add('border-top');
-          previousGameId = game_id;
-        }
-
-        tableRow.classList.add('border-r-l');
-
-        // Add padding to create spacing between pairs of rows
-        if (i === 0) {
-          tableRow.classList.add('pair-start');
-        }
-
+        const rowBox = document.createElement('div');
+        rowBox.classList.add('row-box');
         
+        var teamCellDiv = document.createElement('div');
+        teamCellDiv.classList.add("cell-div");
+        teamCellDiv.classList.add("team-div");
 
 
-        tableBody.appendChild(tableRow);
+        const teamCell = document.createElement('h3');
+        teamCell.textContent = row.team;
+        teamCellDiv.appendChild(teamCell);
+        rowBox.appendChild(teamCellDiv);
+
+        teamCellDiv = document.createElement('div');
+        teamCellDiv.classList.add("cell-div");
+        const ifWinCell = document.createElement('h3');
+        ifWinCell.textContent = formatCurrency(row.if_win);
+
+        teamCellDiv.appendChild(ifWinCell);
+        rowBox.appendChild(teamCellDiv)
+        
+        teamCellDiv = document.createElement('div');
+        teamCellDiv.classList.add("cell-div");
+
+        const avgOddsCell = document.createElement('h3');
+        avgOddsCell.textContent = "-";
+
+        if(row.average_odds.toString().length > 1){
+          avgOddsCell.textContent = addSign(row.average_odds.toString());
+        }
+        teamCellDiv.appendChild(avgOddsCell);
+        rowBox.appendChild(teamCellDiv);
+
+        betBox.appendChild(rowBox);
       }
+      imgContainer = document.createElement('div');
+      imgContainer.classList.add("row-box");
+      imgContainer.classList.add("img-container");
+
+      img = document.createElement('img');
+      img.classList.add("logo-img");
+      img.src = "static/images/sidebar_logo.png";
+      imgContainer.appendChild(img);
+      betBox.appendChild(imgContainer);
+    tableBody.append(betBox);
     }
+
   });
