271045dd889cd4d5574e1d3bd427307cd9fac2c2
micahcb <micahcb@umich.edu>
2023-11-20 18:03:31 -0700

    adding

--- functionality/db_manager.py
+++ functionality/db_manager.py
@@ -1,12 +1,29 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.pool import QueuePool
+from sshtunnel import SSHTunnelForwarder
 
-DATABASE_URI = 'mysql://user:password@localhost/dbname'  # Replace with your database URI
+# Define your SSH tunnel parameters
+ssh_host = 'ec2-18-221-141-215.us-east-2.compute.amazonaws.com'
+ssh_username = 'ec2-user'
+ssh_pkey_path = '/Users/micahblackburn/Desktop/test_key.pem'
+
+# Define your MySQL database parameters
+mysql_host = '127.0.0.1'  # Localhost because you're tunneling
+mysql_port = 3306  # The port on which the MySQL server is exposed locally
+mysql_database = 'test'
+mysql_uri = f'mysql://user:password@{mysql_host}:{mysql_port}/{mysql_database}'
 
 class DBManager:
     def __init__(self):
-        self.engine = create_engine(DATABASE_URI, pool_pre_ping=True, poolclass=QueuePool)
+        self.tunnel = SSHTunnelForwarder(
+            (ssh_host, 22),
+            ssh_username=ssh_username,
+            ssh_pkey=ssh_pkey_path,
+            remote_bind_address=('database-1.cgsvu7bg7jl5.us-east-2.rds.amazonaws.com', 3306),
+        )
+        self.tunnel.start()  # Start the SSH tunnel
+        self.engine = create_engine(mysql_uri, pool_pre_ping=True, poolclass=QueuePool)
         self.Session = sessionmaker(bind=self.engine)
 
     def create_session(self):
@@ -14,8 +31,9 @@ class DBManager:
 
     def close(self):
         self.engine.dispose()
-
+        self.tunnel.stop()  # Stop the SSH tunnel when closing the connection
     def create_engine(self):
         return self.engine
+
 
 db_manager = DBManager()
