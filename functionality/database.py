0f9f12515a2a1b12f6c2286a6b7a55ed2983778b
youngfeiler <stefantfeiler@gmail.com>
2023-11-30 11:55:57 -0700

    timing issue fixed

--- functionality/database.py
+++ functionality/database.py
@@ -332,7 +332,7 @@ class database():
 
        filtered_df = filtered_df[filtered_df['team'].astype(str).str.strip() != '']
 
-       filtered_df.sort_values(by="snapshot_time", ascending=True, inplace=True)
+       filtered_df.sort_values(by="snapshot_time", ascending=False, inplace=True)
 
        columns_to_compare = ['team']
 
@@ -373,7 +373,7 @@ class database():
 
        if sport == "NFL":
           first_20_rows['time_difference_seconds'] = first_20_rows['time_difference_seconds'] -32400
-       elif sport == "NBA" or sport == "NHL":
+       elif sport == "NBA" or sport == "NHL" or sport == "PREGAME":
           first_20_rows['time_difference_seconds'] = first_20_rows['time_difference_seconds'] -21600
           
         
