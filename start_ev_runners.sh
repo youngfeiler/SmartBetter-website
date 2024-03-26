ebac79c13cc6808f3e5ac6f76ae45cf78ae9d9b0
EC2 Default User <ec2-user@ip-172-31-37-33.us-east-2.compute.internal>
2024-03-26 20:19:19 +0000

    scripts to start and kill ev runners

---
+++ start_ev_runners.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+nohup python3 functionality/pos_ev_runners/nba_pos_ev_runner.py > nba_pos_ev.log &
+
+nohup python3 functionality/pos_ev_runners/nhl_pos_ev_runner.py > nhl_pos_ev.log &
+
+nohup python3 functionality/pos_ev_runners/ncaab_pos_ev_runner.py > ncaab_pos_ev.log &
+
+nohup python3 functionality/pos_ev_copier.py > pos_ev_copier.log &
+
+nohup python3 functionality/pos_ev_runners/pos_ev_compiler.py > pos_ev_compiler.log &
+
+nohup python3 functionality/pos_ev_runners/arb_compiler.py > arb_compiler.log &
+
+echo "EV runners processes started."
