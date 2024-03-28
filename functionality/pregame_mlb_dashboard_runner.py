af518cf9bc059e35f609243b47408714d9830410
youngfeiler <stefantfeiler@gmail.com>
2024-03-27 22:57:38 -0600

    file path

--- functionality/pregame_mlb_dashboard_runner.py
+++ functionality/pregame_mlb_dashboard_runner.py
@@ -129,7 +129,7 @@ class pregame_mlb_dashboard_runner():
 
     def add_extra_info(self, df):
       
-      extra_info = pd.read_csv('mlb_data/')
+      extra_info = pd.read_csv('mlb_data/mlb_extra_info.csv')
 
       df['commence_date'] = pd.to_datetime(pd.to_datetime(df['commence_time']).dt.date).dt.strftime('%Y%m%d')
 
